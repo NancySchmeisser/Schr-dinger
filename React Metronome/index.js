c7001970930f7ffa7a6d5707f5e71e2a5f7ff69c
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-01-15 10:15:06 +0100

    feat
    React Metronome

---
+++ React Metronome/index.js
@@ -0,0 +1,8 @@
+import React from  'react';
+import ReactDOM from 'react-dom';
+import Metronome from './Metronome';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(<Metronome />, document.getElementById('root'));
+registerServiceWorker();
